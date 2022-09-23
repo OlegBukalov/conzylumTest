32ab197a87c26c3b9395d0993e3e6a41eb8e2dca
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2022-09-23 23:37:18 +0300

    feature: added guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProductListComponent} from "./feature/product-list/product-list.component";
 import {LoginPageComponent} from "./core/components/login-page/login-page.component";
 import {NotFoundPageComponent} from "./core/components/not-found-page/not-found-page.component";
+import {AuthGuard} from "./core/services/auth.guard";
 
 const routes: Routes = [
   {path: '', redirectTo: 'products', pathMatch: 'full'},
-  {path: 'products', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginPageComponent},
   {path: '**', component: NotFoundPageComponent}
 ];
