50b2fa7c74d293d5f884656edc4e0736803cad19
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2022-09-24 19:36:10 +0300

    feature: added product list template

--- src/app/feature/product-list/product-item/product-item.component.ts
+++ src/app/feature/product-list/product-item/product-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Product} from "../../../core/models/product.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
-export class ProductItemComponent implements OnInit {
+export class ProductItemComponent {
+  @Input() product: Product;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) {
   }
 
+  onOpenDetails() {
+
+  }
 }
