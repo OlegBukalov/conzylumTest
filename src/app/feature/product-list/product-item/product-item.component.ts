2e96655affc84593881bff53a8dd75e39714c6e1
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2022-10-20 10:36:29 +0300

    feature: added OnPush strategy

--- src/app/feature/product-list/product-item/product-item.component.ts
+++ src/app/feature/product-list/product-item/product-item.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {Product} from "../../../core/models/product.model";
 import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.scss']
+  styleUrls: ['./product-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductItemComponent {
   @Input() product: Product;
